58bf38412ec179782e480376c82ec5f2eebc6465
ElfCode67 <balmesgenrique27@gmail.com>
2026-01-10 18:04:08 +0800

    Learned variables

--- Javascript Fundamentals/index.js
+++ Javascript Fundamentals/index.js
@@ -1,2 +1,8 @@
-document.getElementById("myh1").textContent = `Hello`;
-document.getElementById("myP").textContent = `Hello i like pizza`;
+let fullName = "Genrique Balmes";
+let age = 21;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}.`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `Are you a student?${isStudent}`;
+
